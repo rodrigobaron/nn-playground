bff597f6bc1c35758a6995681a3776234c7f4729
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-03-24 15:04:37 -0300

    add relu layer

--- playground/nn/__init__.py
+++ playground/nn/__init__.py
@@ -2,5 +2,8 @@ from .tanh import Tanh
 from .mse_loss import MSELoss
 from .linear import Linear
 from .model import Model
+from .relu import Relu
 
-__all__ = ['Tanh', 'MSELoss', 'Linear', 'Model']
+
+__all__ = ['Tanh', 'MSELoss', 'Linear', 'Model',
+           'Relu']
---
+++ playground/nn/relu.py
@@ -0,0 +1,16 @@
+from .activation import Activation
+import numpy as np
+
+class Relu(Activation):
+    """TODO: Relu docs"""
+
+    def __init__(self):
+        super().__init__()
+
+    def f(self, inputs):
+        return np.maximum(inputs, 0)
+
+    def f_prime(self, inputs):
+        ret = inputs.copy()
+        ret[inputs < 0] = 0
+        return ret
