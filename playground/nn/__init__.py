f6397b95b84fbaffaa153910ea3b28b857800b45
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-03-24 15:19:44 -0300

    add sigmoid layer

--- playground/nn/__init__.py
+++ playground/nn/__init__.py
@@ -4,7 +4,8 @@ from .linear import Linear
 from .model import Model
 from .relu import ReLU
 from .leaky_relu import LeakyReLU
+from .sigmoid import Sigmoid
 
 
 __all__ = ['Tanh', 'MSELoss', 'Linear', 'Model',
-           'ReLU', 'LeakyReLU']
+           'ReLU', 'LeakyReLU', 'Sigmoid']
---
+++ playground/nn/sigmoid.py
@@ -0,0 +1,14 @@
+from .activation import Activation
+import numpy as np
+
+class Sigmoid(Activation):
+    """TODO: Sigmoid docs"""
+
+    def __init__(self):
+        super().__init__()
+
+    def f(self, inputs):
+        return 1 / (1 + np.exp(-inputs))
+
+    def f_prime(self, inputs):
+        self.f(inputs) * (1 - self.f(inputs))
