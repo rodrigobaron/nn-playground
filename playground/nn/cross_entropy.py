fix crosse_ntropy and softmax

playground/nn/cross_entropy.py
@@ -10,10 +10,11 @@ class CrossEntropy(Loss):
 
     def loss(self, predicted, truth):
         m = predicted.shape[0]
-        p = self.softmax(predicted)
-        llik = -np.log(p[range(m), np.argmax(truth)])
 
-        _loss = np.sum(llik) / m
+        prob = self.softmax(predicted)
+        llike = -np.log(prob[range(m), truth])
+
+        _loss = np.sum(llike) / m
 
         if self.regularization is None:
             return _loss
@@ -22,8 +23,7 @@ class CrossEntropy(Loss):
 
     def grad(self, predicted, truth):
         m = predicted.shape[0]
-        grad_y = self.softmax(predicted)
-        grad_y[range(m), np.argmax(truth)] -= 1.
-        grad_y /= m
+        prob = self.softmax(predicted)
+        prob[range(m), truth] -= 1.
 
-        return grad_y
+        return prob / m

playground/utils/softmax.py
@@ -3,8 +3,8 @@ import numpy as np
 class Softmax:
     """TODO: Softmax docs"""
     def forward(self, inputs):
-        ex = np.exp((inputs.T - np.max(inputs)).T)
-        return (ex.T / ex.sum()).T
+        ex = np.exp((inputs.T - np.max(inputs, axis=1)).T)
+        return (ex.T / ex.sum(axis=1)).T
 
     def __call__(self, inputs):
         return self.forward(inputs)