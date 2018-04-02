3c25f0915de2dff6e18294f1084c6154bb73e0a0
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-04-02 10:52:54 -0300

    add NesterovMomentum

--- playground/optim/__init__.py
+++ playground/optim/__init__.py
@@ -1,4 +1,5 @@
 from .sgd import SGD
 from .sgd_momentum import SGDMomentum
+from .nesterov_momentum import NesterovMomentum
 
-__all__ = ['SGD', 'SGDMomentum']
+__all__ = ['SGD', 'SGDMomentum', 'NesterovMomentum']
