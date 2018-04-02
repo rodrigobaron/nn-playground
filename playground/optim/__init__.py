bdd2ee1cbab27a30250d351a6ad728d4d123fd94
Rodrigo Baron <baron.rodrigo0@gmail.com>
2018-04-02 11:01:53 -0300

    add Adagrad

--- playground/optim/__init__.py
+++ playground/optim/__init__.py
@@ -1,5 +1,6 @@
 from .sgd import SGD
 from .sgd_momentum import SGDMomentum
 from .nesterov_momentum import NesterovMomentum
+from .adagrad import Adagrad
 
-__all__ = ['SGD', 'SGDMomentum', 'NesterovMomentum']
+__all__ = ['SGD', 'SGDMomentum', 'NesterovMomentum', 'Adagrad']
